b91b78d9642494bfedd626efc543899ba222f9a7
Ali Ahsan <136238207+aliahsan08@users.noreply.github.com>
2025-07-22 23:26:39 +0500

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,4 @@
 import dash
-import os
 from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -14,8 +13,45 @@ navbar = dbc.NavbarSimple(
     brand="SuperStore Sales Analytics Dashboard",
     fluid=True,
 )
+
+card =  dbc.Row([
+            dbc.Col(
+                dbc.Card([
+                    dbc.CardHeader(f"Total Sales (in $)"),
+                    dbc.CardBody([
+                        html.Br(),html.Div(id = 'sales-value'),html.Br()
+                    ],id = 'total-sales')
+                ],className="bg-dark text-white shadow-lg rounded mb-4,",style={'textAlign':'center'})
+            ),
+            dbc.Col(
+                dbc.Card([
+                    dbc.CardHeader('Total Profit (in $)'),
+                    dbc.CardBody([
+                        html.Br(),html.Div(id = 'profit-value'),html.Br()
+                    ],id='total-profit')
+                ],className = "bg-light text-dark shadow-lg rounded mb-4",style={'textAlign':'center'})
+            ),
+            dbc.Col(
+                dbc.Card([
+                    dbc.CardHeader('Highest Profiting State (in $)'),
+                    dbc.CardBody([
+                        html.Br(),html.Div(id = 'profiting-state'),html.Br()
+                    ])
+                ],style={
+                    'textAlign':'center',
+                    "backgroundColor": "#dbeafe",
+                    "color": "#1e3a8a",
+                    "boxShadow": "0 4px 12px rgba(0, 0, 0, 0.1)",
+                    "borderRadius": "5px",
+                    "marginBottom": "1.5rem"
+                    })
+            ),
+    ])
+
 app.layout = html.Div([
     navbar,
+    html.Br(),html.Br(),
+    card,
     html.Br(),
     dbc.Row([
         dbc.Label("Select Year Range",style={'textAlign':'center'}),
@@ -47,7 +83,8 @@ app.layout = html.Div([
         dbc.Col([
             dcc.Graph(id='bar')
         ],width = 6),
-    ])
+    ]),
+    html.Br()
 ])
 @app.callback(
     Output('map', 'figure'),
@@ -55,7 +92,7 @@ app.layout = html.Div([
     Input('year-slider', 'value'),
     Input('dropdown', 'value')
 )
-def makeGraph(selected_year,figure_data):
+def updateGraph(selected_year,figure_data):
     filtered_df = df[(df['Year'] >= selected_year[0]) & (df['Year'] <= selected_year[1])]
     grouped_df = filtered_df.groupby(['State','State Abbrev'],as_index = False)[figure_data].sum()
     fig = px.choropleth(
@@ -72,9 +109,30 @@ def makeGraph(selected_year,figure_data):
     fig2 = px.bar(
         grouped_df,
         x = 'State',
-        y = grouped_df[figure_data]
+        y = grouped_df[figure_data],
+        title=f"SuperStore {figure_data} in {selected_year[0]} - {selected_year[1]} "
+    )
+    fig2.update_layout(
+        plot_bgcolor='#d5ebf6',
+        paper_bgcolor='#FFFFFF',
     )
     return fig,fig2
+
+@app.callback(
+    Output('sales-value', 'children'),
+    Output('profit-value', 'children'),
+    Output('profiting-state', 'children'),
+    Input('year-slider', 'value'),
+)
+def updateValues(selected_year):
+    filtered_df = df[(df['Year'] >= selected_year[0]) & (df['Year'] <= selected_year[1])]
+    sales_df = filtered_df.groupby(['State'],as_index = False)['Sales'].sum()
+    profit_df = filtered_df.groupby(['State'],as_index = False)['Profit'].sum()
+    profiting_state = f"{profit_df.loc[profit_df['Profit'].idxmax(), 'State']}: {round(profit_df['Profit'].max(),2)}"
+
+    return round(sales_df['Sales'].sum(),2),round(profit_df['Profit'].sum(),2),profiting_state
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
     app.run(host="0.0.0.0", port=port, debug = False)
